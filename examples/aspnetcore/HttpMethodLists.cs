ba5e61c670c9d780949cb6b7ac06b52086ba27db
cijothomas <cijo.thomas@gmail.com>
2026-05-16 13:16:58 -0700

    Move HttpMethodLists to its own file to satisfy SA1649

---
+++ examples/aspnetcore/HttpMethodLists.cs
@@ -0,0 +1,8 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+internal static class HttpMethodLists
+{
+    public static readonly string[] HttpHead = { "HEAD" };
+    public static readonly string[] HttpOptions = { "OPTIONS" };
+}
